07383447ec2d5ac43f12d23e371c93d95a69d89c
Mario <mariobielborba@hotmail.com>
2024-10-21 21:42:58 -0300

    Alterado valor fixo do tamanho da senha, e adicionado input para usuario preencher a quantidade.

--- gerador-senha/src/App.jsx
+++ gerador-senha/src/App.jsx
@@ -1,39 +1,47 @@
-import { useState } from "react"
+import { useState } from "react";
 
 function App() {
-
-  const [password, setPassword] = useState("")
-  const [copyText, setCopyText] = useState("Copiar")
+  const [password, setPassword] = useState("");
+  const [copyText, setCopyText] = useState("Copiar");
+  const [passwordSize, setPasswordSize] = useState(12);
 
   function generate() {
-    const characters = "'1234567890-=!@#$%¨&*()_+qwertyuiop[asdfghjklç~]zxcvbnm,.;/QWERTYUIOP{ASDFGHJKLÇ^}ZXCVBNM<>:?"
-    const length = 12;
-    let newPassword = ""
+    const characters =
+      "'1234567890-=!@#$%¨&*()_+qwertyuiop[asdfghjklç~]zxcvbnm,.;/QWERTYUIOP{ASDFGHJKLÇ^}ZXCVBNM<>:?";
+  
+    let newPassword = "";
 
-    for (let i = 0; i < length; i++) {
-        const position = Math.floor(Math.random() * characters.length)
-        newPassword += characters[position]
+    for (let i = 0; i < passwordSize; i++) {
+      const position = Math.floor(Math.random() * characters.length);
+      newPassword += characters[position];
     }
 
-    setPassword(newPassword)
+    setPassword(newPassword);
 
-    setCopyText("Copiar")
-  
+    setCopyText("Copiar");
   }
 
   function copyToClipboar() {
-    window.navigator.clipboard.writeText(password)
-    setCopyText("Copiado!")
+    window.navigator.clipboard.writeText(password);
+    setCopyText("Copiado!");
   }
 
   return (
-   <div className="app">
-    <h1>Gerador de Senhas</h1>
-    <button onClick={generate}>Gerar!</button>
-    <button onClick={copyToClipboar}>{copyText}</button>
-    <div className="passwordDiv">{password}</div>
-   </div>
-  )
+    <div className="app">
+      <h1>Gerador de Senhas</h1>
+      <div>
+        <label htmlFor="passwordSize">Tamanho:</label>
+        <input type="number"
+         id="passwordSize"
+          min={1} 
+          value={passwordSize}
+          onChange={(ev) => setPasswordSize(ev.target.value)} />
+      </div>
+      <button onClick={generate}>Gerar senha de {passwordSize} caracteres!</button>
+      <button onClick={copyToClipboar}>{copyText}</button>
+      <div className="passwordDiv">{password}</div>
+    </div>
+  );
 }
 
-export default App
+export default App;
